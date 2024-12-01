c6b901e384111fe52516a964a21f94c688dcf335
yoshihiro-ri <ri.dev1027@gmail.com>
2024-12-01 15:14:50 +0900

    lint

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,16 @@ import { scrape } from "./scrape";
 const app = new Hono();
 
 app.get("/", async (c) => {
-
   const result = await send();
   console.log(result);
   console.log("===================");
-  return c.json(result)
+  return c.json(result);
 });
 
 app.get("/scrape", async (c) => {
-  const result = await scrape()
+  const result = await scrape();
   console.log(result);
-  return c.json(result)
+  return c.json(result);
 });
 
 // const waitSend = async () => {
--- src/send.ts
+++ src/send.ts
@@ -4,22 +4,24 @@ import { CHANNEL_ID } from "../env";
 
 import { scrape } from "./scrape";
 export const send = async (): Promise<object> => {
-  const  articles = await scrape()
-  const  article = articles[0]
+  const articles = await scrape();
+  const article = articles[0];
   const params = new URLSearchParams();
   params.append("channel", CHANNEL_ID);
-  params.append("text", `${article.title}   \n❤️${article.iine} \n${article.link}`,);
+  params.append(
+    "text",
+    `${article.title}   \n❤️${article.iine} \n${article.link}`
+  );
 
   const response = await fetch(SLACK_LINK, {
     method: "POST",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
-      "Authorization": `Bearer ${BOT_OAUTH_TOKEN}`
+      Authorization: `Bearer ${BOT_OAUTH_TOKEN}`,
     },
-    body: params
+    body: params,
   });
   console.log("Response body:", await response.text());
 
   return await response;
-}
-
+};
